362905186969b71614250e9061a164cf18956111
gefgu <gefgu@hotmail.com>
2022-07-03 20:57:50 -0300

    Add form page with routing

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
 module.exports = router;
