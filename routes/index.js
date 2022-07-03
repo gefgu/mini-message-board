fbbbb0488edacd8cad7bd11ea18ea607281e9856
gefgu <gefgu@hotmail.com>
2022-07-03 16:06:35 -0300

    Add sample messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  { test: "Hi there!", user: "Amando", added: new Date() },
+  { test: "Hello World!", user: "Charles", added: new Date() },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
